f07c28e6a6793453781f607b73b88645fa2f456b
Meira-JH <jhalvesmeira@gmail.com>
2020-06-23 13:17:34 -0300

    pequenas alterações

--- cookenu/src/business/UserBusiness.ts
+++ cookenu/src/business/UserBusiness.ts
@@ -5,34 +5,27 @@ import { validatePassword } from "../Util/validatePassword";
 import { HashManager } from "../services/HashManager";
 
 export class UserBusiness {
+  private userDataBase = new UserDatabase();
 
-    private userDataBase = new UserDatabase();
-
-    public async signup(
-        id : string,
-        name : string,
-        password : string,
-        email : string,
-        device : string
-    ) {
-        const isEmail = validateEmail(email);
-        if (!isEmail) {
-          throw new CustomError("Email inválido, parça", 412);
-        }
-      
-        const isPassword = validatePassword(password);
-        if (!isPassword) {
-          throw new CustomError("Senha inválida, campeão!", 412);
-        }
-
-        const newHash = await new HashManager().createHash(password);
-        console.log("business", id, name, newHash, email, device)
-        await this.userDataBase.signup(
-            id,
-            name,
-            newHash,
-            email,
-            device
-        )
+  public async signup(
+    id: string,
+    name: string,
+    password: string,
+    email: string,
+    device: string
+  ) {
+    const isEmail = validateEmail(email);
+    if (!isEmail) {
+      throw new CustomError("Email inválido!", 412);
     }
+
+    const isPassword = validatePassword(password);
+    if (!isPassword) {
+      throw new CustomError("Senha inválida!", 412);
+    }
+
+    const newHash = await new HashManager().createHash(password);
+
+    await this.userDataBase.signup(id, name, newHash, email, device);
+  }
 }
--- cookenu/src/controller/UserController.ts
+++ cookenu/src/controller/UserController.ts
@@ -6,52 +6,40 @@ import { RefreshTokenDataBase } from "../data/RefreshTokenDataBase";
 import { ServerDataBase } from "../data/ServerDataBase";
 
 export class UserController {
+  async signup(request: Request, response: Response) {
+    const newId = new IdGenerator().generate();
+    const { name, email, password, device } = request.body;
 
-    async signup(
-        request: Request,
-        response: Response
-    ) {
-        const newId = new IdGenerator().generate();
-        const { name, email, password, device } = request.body;
-        
-        await new UserBusiness().signup(
-          newId,
-          name,
-          password,
-          email,
-          device
-        );
+    await new UserBusiness().signup(newId, name, password, email, device);
 
-        console.log("indo pro access token")
+    const newAccessToken = new Authenticator().generateToken(
+      {
+        id: newId,
+      },
+      "1d"
+    );
 
-        const newAccessToken = new Authenticator().generateToken(
-          {
-            id: newId
-          },
-          "1d"
-        );
-      
-        const newRefreshToken = new Authenticator().generateToken(
-          {
-            id: newId,
-            device: device,
-          },
-          process.env.REFRESH_TOKEN_EXPIRES_IN
-        );
-      
-        await new RefreshTokenDataBase().storeRefreshToken(
-          newRefreshToken,
-          device,
-          true,
-          newId
-        );
-      
-        response.status(200).send({
-          message: "Usuário criado com sucesso!",
-          'token de acesso': newAccessToken,
-          'refresh token': newRefreshToken
-        });
-      
-        await ServerDataBase.destroyConnection();    
-    }
+    const newRefreshToken = new Authenticator().generateToken(
+      {
+        id: newId,
+        device: device,
+      },
+      process.env.REFRESH_TOKEN_EXPIRES_IN
+    );
+
+    await new RefreshTokenDataBase().storeRefreshToken(
+      newRefreshToken,
+      device,
+      true,
+      newId
+    );
+
+    response.status(200).send({
+      message: "Usuário criado com sucesso!",
+      "token de acesso": newAccessToken,
+      "refresh token": newRefreshToken,
+    });
+
+    await ServerDataBase.destroyConnection();
+  }
 }
