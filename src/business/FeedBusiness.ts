28840825f5666833e54d14866fa5a8566bc21231
Meira-JH <jhalvesmeira@gmail.com>
2020-06-25 15:16:59 -0300

    pegar feed funcionando

--- src/business/FeedBusiness.ts
+++ src/business/FeedBusiness.ts
@@ -1,15 +1,8 @@
 import { FeedDataBase } from "src/data/FeedDataBase";
 
-
 export class FeedBusiness {
-
-    async getFeed ( friendsList : any[] ){
-        let feed : any[] = [];
-        let friend
-
-        for ( friend of friendsList ){
-            const post : any = await new FeedDataBase().getFeed( friend )
-            feed.push(post)
-        }
-    }
+  async getFeed(userId: string) {
+    const feed = await new FeedDataBase().getFeed(userId);
+    return feed;
+  }
 }
--- src/controller/FeedController.ts
+++ src/controller/FeedController.ts
@@ -1,26 +1,15 @@
 import { Request, Response } from "express";
-import { Friendship } from "src/data/FriendshipDataBase";
 import { Authenticator } from "src/services/Authenticator";
 import { FeedBusiness } from "@business/FeedBusiness";
-import { FeedDataBase } from "src/data/FeedDataBase";
-
-
 
 export class FeedController {
+  async getFeed(request: Request, response: Response) {
+    const user = new Authenticator().getData(
+      request.headers.authorization as string
+    );
 
-    async getFeed ( request : Request, response : Response){
+    const feed = await new FeedBusiness().getFeed(user.id);
 
-        const user = new Authenticator().getData( 
-            request.headers.authorization as string
-         )
-
-        const friendsList = await new FeedDataBase().getAllFriends( user.id )
-
-        const feed = await new FeedBusiness().getFeed( 
-            friendsList
-        )
-
-        response.status(200).send( feed )
-
-    }
+    response.status(200).send(feed);
+  }
 }
