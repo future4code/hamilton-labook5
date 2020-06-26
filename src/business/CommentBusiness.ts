8bd30ed5cc2a48ff05ac3284c4d0c5f784ae1af0
Meira-JH <jhalvesmeira@gmail.com>
2020-06-26 16:57:24 -0300

    PR

--- src/business/CommentBusiness.ts
+++ src/business/CommentBusiness.ts
@@ -2,11 +2,9 @@ import { CommentDTO } from "src/model/Comment";
 import { CommentDataBase } from "src/data/CommentDataBase";
 
 export class CommentBusiness {
-  
   private commentDataBase = new CommentDataBase();
 
-  public async postComment( newComment : CommentDTO) {
-    
-    await this.commentDataBase.postComment( newComment )
+  public async postComment(newComment: CommentDTO) {
+    await this.commentDataBase.postComment(newComment);
   }
 }
